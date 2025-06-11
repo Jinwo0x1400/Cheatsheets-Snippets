f66f552226cf4f2df45ccd8fbf31e0b95d2b9a21
Jinwoo99 <adityamyname07@gmail.com>
2025-06-11 14:08:38 +0700

    Create PHP-Snippets.php

---
+++ PHP-Snippets.php
@@ -0,0 +1,12 @@
+// Dump and die
+dd($var);
+
+// Ambil IP user
+$_SERVER['REMOTE_ADDR'];
+
+// Cek string mengandung substring
+if (strpos($haystack, $needle) !== false) {}
+
+// JSON encode + decode
+$data = json_decode($json, true);
+$json = json_encode($array);
